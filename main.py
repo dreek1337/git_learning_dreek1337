450900b0472daa3378429039b76dd9a8a580b4b5
danil.sapog19@gmail.com <saf1337@DESKTOP-G4LPVNB.localdomain>
2023-01-13 14:56:32 +0300

    Проверка скорости циклов

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-print('Жопа')
-# Yaho
+from time import time
 
 a = 10
+start = time()
 while True:
     a -= 1
-
     if a:
-        print('Hi')
+        b = a**3211 * a
     else:
         break
-        
+print(time() - start)
+
+
+start_2 = time()
+for i in range(10):
+    b = i**3211 * i
+print(time() - start_2)
+
+
+start_3 = time()
+b = [i**3211 * i for i in range(10)]
+print(time() - start_3)
